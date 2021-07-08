a99b78cde17184136a57691ce29cbf26998b2aa1
tusharlalawat <tk3850132@gmail.com>
2021-07-08 15:14:58 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+document.querySelector('#btn').onclick = function () {
+
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("please enter a task");
+    }
+
+    else {
+        document.querySelector('#tasks').innerHTML
+            += `
+                <div class="task">
+                    <div>
+                        <button class="check"><i class="fas fa-check-square"></i></button>
+                        <span id ="taskname">
+                            ${document.querySelector('#newtask input').value}
+                        </span>
+                    </div>
+                    <button class="delete">
+                        <i class="far fa-trash-alt"></i> 
+                    </button>
+                </div>
+            `;
+
+        var current = document.querySelectorAll(".delete");
+        for (var i = 0; i < current.length; i++) {
+            current[i].onclick = function () {
+                this.parentNode.remove();
+            } 
+        }
+
+        var taskcompleted = document.querySelectorAll(".check");
+        for (var i = 0; i < taskcompleted.length; i++) {
+            taskcompleted[i].onclick = function () {
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector('#newtask input').value = "";
+    }
+}
